5064bac1745540389596f6d87d04541e5462f2fa
Gabriel Florit <gabrielflorit@gmail.com>
2013-06-07 16:14:31 -0400

    getting ready to rework AreaChart into … TimeAreaChart?!

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 (function() {
 
-	var data = [0, 1, 2, 3, 8, 3, 1, 0, 2, 1, 0];
+	$.getJSON('data.json', function(json) {
 
-	var area = d3.select('#container')
-		.append('svg')
-		.chart('AreaChart')
-		// .width(500)
-		// .height(300)
-		// .padding(10)
-		.draw(data);
+		var data = [0, 1, 2, 3, 8, 3, 1, 0, 2, 1, 0];
 
-	var bottomAxis = d3.select('#container')
-		.append('svg')
-		.chart('XAxisChart')
-		// .width(500)
-		// .height(300)
-		// .padding(10)
-		.draw(data);
+		var area = d3.select('#container')
+			.append('svg')
+			.chart('AreaChart')
+			// .width(500)
+			// .height(300)
+			// .padding(10)
+			.draw(data);
+
+		var bottomAxis = d3.select('#container')
+			.append('svg')
+			.chart('XAxisChart')
+			// .width(500)
+			// .height(300)
+			// .padding(10)
+			.draw(data);
+
+	});
 
 }());
