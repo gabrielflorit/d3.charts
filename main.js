84c2714139323e62a050051506d7efdbd4331b49
Gabriel Florit <gabrielflorit@gmail.com>
2013-06-07 16:06:32 -0400

    added xAxis chart - it's a copy of AreaChart for now

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 (function() {
 
 	var data = [0, 1, 2, 3, 8, 3, 1, 0, 2, 1, 0];
+
 	var area = d3.select('#container')
 		.append('svg')
 		.chart('AreaChart')
 		// .width(500)
 		// .height(300)
 		// .padding(10)
-		;
+		.draw(data);
 
-	area.draw(data);
+	var bottomAxis = d3.select('#container')
+		.append('svg')
+		.chart('XAxisChart')
+		// .width(500)
+		// .height(300)
+		// .padding(10)
+		.draw(data);
 
 }());
