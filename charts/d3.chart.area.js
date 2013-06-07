296adef45c3cf74b1a74fd4e6534a0897d4b35e8
Gabriel Florit <gabrielflorit@gmail.com>
2013-06-07 15:49:32 -0400

    trying to get padding working

--- charts/d3.chart.area.js
+++ charts/d3.chart.area.js
@@ -13,24 +13,41 @@ d3.chart('BaseChart').extend('AreaChart', {
 		// create the area generator
 		this.area = d3.svg.area()
 			.x(function(d, i) { return x(i); })
-			.y0(this.height())
 			.y1(function(d) { return y(d); });
 
+		function setPadding(element, padding) {
+			element.attr({
+				transform: 'translate(' + padding/2 + ', ' + padding/2 + ')'
+			});
+		}
+		var areaLayerBase = this.base.append('g');
+		setPadding(areaLayerBase, this.padding());
+
+		function setHeight(chart) {
+			chart.area.y0(chart.height() - chart.padding());
+			chart.y.range([chart.height() - chart.padding(), 0]);
+		}
+		setHeight(this);
+
 		// reset scale ranges on dimension changes
-		chart.on('change:width', function(newWidth) {
-			chart.x.range([0, newWidth]);
+		chart.on('change:width', function() {
+			chart.x.range([0, this.width() - this.padding()]);
 		});
-		chart.on('change:height', function(newHeight) {
-			this.area.y0(this.height());
-			chart.y.range([newHeight, 0]);
+		chart.on('change:height', function() {
+			setHeight(chart);
+		});
+		chart.on('change:padding', function() {
+			chart.trigger('change:width');
+			chart.trigger('change:height');
+			setPadding(this.layer('area'), this.padding());
 		});
 
-		this.layer('area', this.base.append('g'), {
+		this.layer("area", areaLayerBase, {
 
 			dataBind: function(data) {
 				var chart = this.chart();
 
-				chart.x.domain([0, data.length]);
+				chart.x.domain([0, data.length - 1]);
 				chart.y.domain(d3.extent(data));
 
 				return this.selectAll('area').data([data]);
@@ -43,6 +60,31 @@ d3.chart('BaseChart').extend('AreaChart', {
 			}
 
 		});
+	},
+
+	padding: function(newPadding) {
+		if (arguments.length === 0) {
+			return this._padding || 50;
+		}
+
+		var oldPadding = this._padding;
+
+		this._padding = newPadding;
+
+		// only if the padding actually changed:
+		if (this._padding !== oldPadding) {
+
+			// trigger a change event
+			this.trigger("change:padding");
+
+			// redraw if we saved the data on the chart
+			if (this.data) {
+				this.draw(this.data);
+			}
+		}
+
+		// always return the chart, for chaining magic.
+		return this;
 	}
 
 });
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 (function() {
 
-	var data = [1, 2, 3, 8, 3, 1, 0, 2, 1];
+	var data = [0, 1, 2, 3, 8, 3, 1, 0, 2, 1, 0];
 	var area = d3.select('#container')
 		.append('svg')
 		.chart('AreaChart')
-		.width(500)
-		.height(300);
+//		 .width(500)
+		 .height(300)
+		.padding(10)
+		;
 
 	area.draw(data);
 
